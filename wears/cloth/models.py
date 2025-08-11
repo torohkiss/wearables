6ab8cdd4700b51a8592a4d4e03f9de6f883ec81e
torohkiss <torohkiss@yahoo.com>
2025-08-11 11:20:06 +0100

    made new migrations after correction in models

--- wears/cloth/models.py
+++ wears/cloth/models.py
@@ -33,7 +33,7 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=10, decimal_places=2)
     available = models.BooleanField(default=True)
     created = models.DateTimeField(auto_now_add=True)
-    update = models.DateTimeField(auto_now=True)
+    updated = models.DateTimeField(auto_now=True)
     
     class Meta:
         ordering = ['name']
